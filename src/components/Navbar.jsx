ea2509f64b78e06cd57c3027923bb69e0592ce60
Anastasia <anastasiia.dontsu@gmail.com>
2023-12-13 19:44:08 +0200

    changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Navbar(){
+ return(
+<div className='flex justify-between item-center h-24'>
+<ul className="flex">
+    <li className="p-4">Home</li>
+    <li className="p-4">Home</li>
+    <li className="p-4">Home</li>
+    <li className="p-4">Home</li>
+</ul>
+</div>
+ )
+}
+export default Navbar
