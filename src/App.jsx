2fb153c5b0d70611d46f14357052f7166e19df7b
Anastasia <anastasiia.dontsu@gmail.com>
2023-12-15 16:21:49 +0200

    add file Yourprofile

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Technology from "./components/Technology";
-import About from "./components/About";
+import Navbar from "./components/Navbar.jsx";
+import Technology from "./components/Technology.jsx";
+import About from "./components/About.jsx";
+import Yourprofile from "./components/Yourprofile.jsx";
 const App = () => {
   return (
     <div>
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Navbar />}>
           <Route path="/technology" element={<Technology />} />
           <Route path="/about" element={<About />} />
+          <Route path="/your-profile" element={<Yourprofile />} />
         </Route>
       </Routes>
     </div>
