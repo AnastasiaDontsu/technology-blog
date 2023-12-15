7cd2c09489eb614928ed665f87d6722f238754bb
Anastasia <anastasiia.dontsu@gmail.com>
2023-12-15 16:08:31 +0200

    add 2 Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Technology from "./components/Technology";
+import About from "./components/About";
 const App = () => {
- 
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<Navbar />}>
+          <Route path="/technology" element={<Technology />} />
+          <Route path="/about" element={<About />} />
+        </Route>
       </Routes>
     </div>
   );
