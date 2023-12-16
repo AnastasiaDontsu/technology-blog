107f9d44d830795897a2e24c777773aa65c1718f
Anastasia <anastasiia.dontsu@gmail.com>
2023-12-16 03:54:27 +0200

    add route

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      GGcjnknlllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllld;fc
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Yourprofile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Yourprofile() {
+  return <div>GG</div>;
+}
+
+export default Yourprofile;
