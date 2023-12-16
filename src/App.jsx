14a1630d457f5875617da86108f5f995184f0c7b
Anastasia <anastasiia.dontsu@gmail.com>
2023-12-16 03:41:58 +0200

    changed Route

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Navbar from "./components/Navbar.jsx";
 import Technology from "./components/Technology.jsx";
 import About from "./components/About.jsx";
 import Yourprofile from "./components/Yourprofile.jsx";
+import Signout from "./components/Signout.jsx";
+
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Navbar />}>
-          <Route path="/technology" element={<Technology />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/your-profile" element={<Yourprofile />} />
-        </Route>
+        <Route path="/technology" element={<Technology />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/your-profile" element={<Yourprofile />} />
+        <Route path="/Sign-out" element={<Signout />} />
       </Routes>
     </div>
   );
