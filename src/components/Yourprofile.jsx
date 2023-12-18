32b3f8f90a19b3fad063e1ee2475772f905cc9b3
Anastasia <anastasiia.dontsu@gmail.com>
2023-12-18 23:20:31 +0200

    I'm changing my registration

--- src/components/Yourprofile.jsx
+++ src/components/Yourprofile.jsx
@@ -1,7 +1,169 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 function Yourprofile() {
-  return <div>GG</div>;
+  const schema = yup.object().shape({
+    firstName: yup
+      .string()
+      .matches(/^([0-9]*)$/, "First Name should not contain numbers")
+      .required("First Name is a required field"),
+    lastName: yup
+      .string()
+      .matches(/^([0-9]*)$/, "Last Name should not contain numbers")
+      .required("Last Name is a required field"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <div className="bg-white dark:bg-gray-900">
+      <div className="flex justify-center min-h-screen">
+        <div
+          className="hidden bg-cover lg:block lg:w-2/5"
+          style={{
+            backgroundImage:
+              "url('https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80')",
+          }}
+        ></div>
+        <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
+          <div className="w-full">
+            <h1 className="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
+              Get your free account now.
+            </h1>
+
+            <p className="mt-4 text-gray-500 dark:text-gray-400">
+              Let’s get you all set up so you can verify your personal account
+              and begin setting up your profile.
+            </p>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2"
+            >
+              <div>
+                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                  First Name
+                </label>
+                <input
+                  {...register("name", {
+                    required: true,
+                  })}
+                  placeholder="First Name"
+                  errors={!!errors.firstName}
+                  helperText={errors.firstName?.message}
+                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                />
+              </div>
+
+              <div>
+                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                  Last name
+                </label>
+                <input
+                  {...register("Lastname", {
+                    required: true,
+                  })}
+                  placeholder=" Last name"
+                  errors={!!errors.Lastname}
+                  helperText={errors.Lastname?.message}
+                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                />
+              </div>
+
+              <div>
+                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                  Phone number
+                </label>
+                <input
+                  type="text"
+                  placeholder="XXX-XX-XXXX-XXX"
+                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                />
+              </div>
+
+              <div>
+                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                  Email address
+                </label>
+                <input
+                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                  {...register("email", {
+                    required: "Email is a required field",
+                    pattern: {
+                      value: /[A-Za-z]{3}/,
+                      message: "Email is not a regular ",
+                    },
+                  })}
+                  placeholder="Email"
+                />
+                {errors.email && (
+                  <div style={{ color: "red" }}>{errors.email.message}</div>
+                )}
+              </div>
+
+              <div>
+                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                  Password
+                </label>
+                <input
+                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                  {...register("name", {
+                    required: "Name is a required field",
+                  })}
+                  placeholder="Name"
+                />
+                {errors.name && (
+                  <div style={{ color: "red" }}>{errors.name.message}</div>
+                )}
+              </div>
+
+              <div>
+                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                  Confirm password
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                />
+              </div>
+
+              <button className="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                <span>Sign Up </span>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5 rtl:-scale-x-100"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Yourprofile;
