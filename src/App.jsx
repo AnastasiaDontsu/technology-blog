be3b27f7c39793cd87c3f3c4cd248e1b7ce66f33
Anastasia <anastasiia.dontsu@gmail.com>
2023-12-19 01:33:56 +0200

    new Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
-import Technology from "./components/Technology.jsx";
+
 import About from "./components/About.jsx";
 import Yourprofile from "./components/Yourprofile.jsx";
 import Signout from "./components/Signout.jsx";
@@ -11,7 +11,6 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/technology" element={<Technology />} />
         <Route path="/about" element={<About />} />
         <Route path="/your-profile" element={<Yourprofile />} />
         <Route path="/Sign-out" element={<Signout />} />
