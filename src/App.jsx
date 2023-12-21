80992e751410689b17a25f3d32487a8054431d8a
Anastasia <anastasiia.dontsu@gmail.com>
2023-12-21 03:04:32 +0200

    added Route <Footer />

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Navbar from "./components/Navbar.jsx";
 import About from "./components/About.jsx";
 import Yourprofile from "./components/Yourprofile.jsx";
 import Signout from "./components/Signout.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Footer />
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/your-profile" element={<Yourprofile />} />
