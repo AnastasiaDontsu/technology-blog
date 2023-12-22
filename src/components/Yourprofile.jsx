573752df3dd228319f7206117330631bc2a7ceaf
Anastasia <anastasiia.dontsu@gmail.com>
2023-12-22 23:10:48 +0200

    fixed import

--- src/components/Yourprofile.jsx
+++ src/components/Yourprofile.jsx
@@ -86,7 +86,7 @@ function Yourprofile() {
                 </label>
                 <input
                   placeholder="Last Name"
-                  {...register("last Name", {
+                  {...register("lastName", {
                     required: true,
                   })}
                   className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
