5fa84719a7e53c69c50fe11583f052df314c399f
Anastasia <anastasiia.dontsu@gmail.com>
2023-12-29 03:20:40 +0200

    add fetch

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/posts/1");
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return <div>{/* Your JSX code here */}</div>;
+}
