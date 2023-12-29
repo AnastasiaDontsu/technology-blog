8a494bc1395b09ed00b27bd32a76dc7e135e01a1
Anastasia <anastasiia.dontsu@gmail.com>
2023-12-29 03:18:58 +0200

    changed Signin

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import About from "./components/About.jsx";
 import Yourprofile from "./components/Yourprofile.jsx";
-import Signup from "./components/ Signup.jsx";
-import Footer from "./components/Footer.jsx";
+import Signin from "./components/ Signin.jsx";
+import Home from "./components/Home.jsx";
+//import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Footer />
+
       <Routes>
         <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
         <Route path="/your-profile" element={<Yourprofile />} />
-        <Route path="/Sign-out" element={<Signup />} />
+        <Route path="/Sign-in" element={<Signin />} />
       </Routes>
     </div>
   );
