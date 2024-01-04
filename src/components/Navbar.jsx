41fb216587a182e485229852805ddfef45ec3268
Anastasia <anastasiia.dontsu@gmail.com>
2024-01-04 16:22:41 +0200

    resize

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar() {
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
+            <div className="relative flex h-[60px] items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -43,7 +43,7 @@ export default function Navbar() {
                           item.current
                             ? "bg-gray-900 text-white"
                             : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium "
+                          "rounded-md px-3 py-2 text-xl font-medium "
                         )}
                         aria-current={item.current ? "page" : undefined}
                       >
@@ -92,7 +92,7 @@ export default function Navbar() {
                             to="/your-profile"
                             className={classNames(
                               active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              "block px-4 py-2 text-xl text-gray-700"
                             )}
                           >
                             Your Profile
@@ -105,7 +105,7 @@ export default function Navbar() {
                             href="Settings"
                             className={classNames(
                               active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              "block px-4 py-2 text-xl text-gray-700"
                             )}
                           >
                             Settings
@@ -118,7 +118,7 @@ export default function Navbar() {
                             href="Sign-in"
                             className={classNames(
                               active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              "block px-4 py-2 text-xl text-gray-700"
                             )}
                           >
                             Sign-in
