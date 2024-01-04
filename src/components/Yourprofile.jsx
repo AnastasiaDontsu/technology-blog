6ce8f1f2a6841c36f7d79e4e50109c44d1d85e2b
Anastasia <anastasiia.dontsu@gmail.com>
2024-01-04 17:07:00 +0200

    add

--- src/components/Yourprofile.jsx
+++ src/components/Yourprofile.jsx
@@ -1,189 +1,204 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import PhoneInput from "react-phone-input-2";
-import "react-phone-input-2/lib/style.css";
-import * as yup from "yup";
 
-function Yourprofile() {
-  const schema = yup.object().shape({
-    firstName: yup
-      .string()
-      .matches(/^[^\d]*$/, "First Name should not contain numbers")
-      .required("First Name is a required field"),
-    lastName: yup
-      .string()
-      .matches(/^[^\d]*$/, "Last Name should not contain numbers")
-      .required("Last Name is a required field"),
-    email: yup
-      .string()
-      .required("Email is a required field")
-      .email("Enter a valid email address"), // Use the email validation provided by Yup
-    password: yup.string().required("Password is a required field"),
-  });
-  const [phone, setPhone] = useState("");
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  contact: "",
+  info: "",
+};
+export default function YourProfile() {
+  const [data, setData] = useState(initialState);
+  const { name, email, password, contact } = data;
+  const [file, setFile] = useState(null);
+  const [progress, setProgress] = useState(null);
+  const [isSubmit, setSubmit] = useState(false);
 
-  const handlePhoneChange = (newPhone) => {
-    setPhone(newPhone);
-  };
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset,
-  } = useForm({
-    mode: "onBlur",
-    resolver: yupResolver(schema),
-  });
+  } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
-    reset();
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
   };
 
   return (
-    <div className="bg-white dark:bg-gray-900">
-      <div className="flex justify-center min-h-screen">
-        <div
-          className="hidden bg-cover lg:block lg:w-2/5"
-          style={{
-            backgroundImage:
-              "url('https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80')",
-          }}
-        ></div>
-        <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
-          <div className="w-full">
-            <h1 className="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
-              Get your free account now.
-            </h1>
-
-            <p className="mt-4 text-gray-500 dark:text-gray-400">
-              Let’s get you all set up so you can verify your personal account
-              and begin setting up your profile.
-            </p>
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2"
+    <section className="bg-white dark:bg-gray-900">
+      <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
+        <form onSubmit={handleSubmit} className="w-full max-w-md">
+          <div className="flex items-center justify-center mt-6">
+            <a
+              href=" sign-in"
+              className="w-1/3 pb-4 font-medium text-center text-gray-500 capitalize border-b dark:border-gray-400 dark:text-gray-300"
             >
-              <div>
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                  First Name
-                </label>
-                <input
-                  placeholder="First Name"
-                  {...register("firstName", {
-                    required: true,
-                  })}
-                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-                {errors.firstName && (
-                  <div style={{ color: "red" }}>{errors.firstName.message}</div>
-                )}
-              </div>
-              <div>
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                  Last Name
-                </label>
-                <input
-                  placeholder="Last Name"
-                  {...register("lastName", {
-                    required: true,
-                  })}
-                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-                {errors.lastName && (
-                  <div style={{ color: "red" }}>{errors.lastName.message}</div>
-                )}
-              </div>
-              <div className="your-container-class">
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                  Phone number
-                </label>
-
-                <PhoneInput
-                  country="us"
-                  value={phone}
-                  onChange={handlePhoneChange}
-                  inputProps={{
-                    style: {
-                      height: "50px",
-                      width: "100%",
-                      backgroundColor: "inherit",
-                      color: "white",
-                    },
-                  }}
-                />
-              </div>
-
-              <div>
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                  Email address
-                </label>
-                <input
-                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300  focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                  {...register("email", {
-                    required: "Email is a required field",
-                    pattern: {
-                      value: /[A-Za-z]{3}/,
-                      message: "Email is not a regular ",
-                    },
-                  })}
-                  placeholder="Email"
-                />
-                {errors.email && (
-                  <div style={{ color: "red" }}>{errors.email.message}</div>
-                )}
-              </div>
-
-              <div>
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                  Password
-                </label>
-                <input
-                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                  {...register("password", {
-                    required: "Name is a required field",
-                  })}
-                  placeholder="Name"
-                />
-                {errors.password && (
-                  <div style={{ color: "red" }}>{errors.password.message}</div>
-                )}
-              </div>
-
-              <div>
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                  Confirm password
-                </label>
-                <input
-                  type="password"
-                  placeholder="Enter your password"
-                  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300  focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-              </div>
-
-              <button className="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                <span>Sign Up </span>
-
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5 rtl:-scale-x-100"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-            </form>
+              sign in
+            </a>
+            <a
+              href="#"
+              className="w-1/3 pb-4 font-medium text-center text-gray-800 capitalize border-b-2 border-blue-500 dark:border-blue-400 dark:text-white"
+            >
+              sign up
+            </a>
           </div>
-        </div>
+
+          <div className="relative flex items-center mt-8">
+            <span className="absolute">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                />
+              </svg>
+            </span>
+
+            <input
+              {...register("name", { required: "Имя обязательно" })}
+              className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              placeholder="name"
+              name="name"
+              onChange={handleChange}
+              value={name}
+              label="Name"
+            />
+            {errors.name && <p>{errors.name.message}</p>}
+          </div>
+          <label
+            htmlFor="dropzone-file"
+            className="flex items-center px-3 py-3 mx-auto mt-6 text-center bg-white border-2 border-dashed rounded-lg cursor-pointer dark:border-gray-600 dark:bg-gray-900"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6 text-gray-300 dark:text-gray-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
+              />
+            </svg>
+            <h2 className="mx-3 text-gray-400">Profile Photo</h2>
+
+            <input
+              id="dropzone-file"
+              type="file"
+              className="hidden"
+              onChange={(e) => setFile(e.target.files[0])}
+              value={file}
+              error={errors.file ? { content: errors.file } : null}
+            />
+          </label>
+
+          <div className="relative flex items-center mt-6">
+            <span className="absolute">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                />
+              </svg>
+            </span>
+
+            <input
+              {...register("email", { required: "Имя обязательно" })}
+              className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              placeholder="email"
+              name="email"
+              onChange={handleChange}
+              value={email}
+              label="Name"
+            />
+            {errors.email && <p>{errors.email.message}</p>}
+          </div>
+
+          <div className="relative flex items-center mt-4">
+            <span className="absolute">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                />
+              </svg>
+            </span>
+
+            <input
+              {...register("password", { required: "Имя обязательно" })}
+              className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              placeholder="password"
+              name="password"
+              onChange={handleChange}
+              value={password}
+              label="Password"
+            />
+            {errors.password && <p>{errors.password.message}</p>}
+          </div>
+
+          <div className="relative flex items-center mt-4">
+            <span className="absolute">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                />
+              </svg>
+            </span>
+
+            <input
+              {...register("contact", { required: "Имя обязательно" })}
+              className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              placeholder="contact"
+              name="contact"
+              onChange={handleChange}
+              value={contact}
+              label="Contact"
+            />
+            {errors.contact && <p>{errors.contact.message}</p>}
+          </div>
+          <div className="mt-6">
+            <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+              Sign Up
+            </button>
+          </div>
+        </form>
       </div>
-    </div>
+    </section>
   );
 }
-
-export default Yourprofile;
