8c9611c15dfa587d9717a1fee79e8d3dd808683e
Anastasia <anastasiia.dontsu@gmail.com>
2024-01-04 16:22:59 +0200

    add fetch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Home() {
-  const [data, setData] = useState([]);
+const Home = () => {
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const getProducts = async () => {
       try {
-        const response = await fetch("https://dummyjson.com/posts/1");
-        const jsonData = await response.json();
-        setData(jsonData);
+        const response = await fetch("https://dummyjson.com/carts/1");
+        const data = await response.json();
+        setProducts(data.products);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
-    fetchData();
-  }, []);
-  return <div>{/* Your JSX code here */}</div>;
-}
+    getProducts();
+  }, []); // Empty dependency array means useEffect will run once after the initial render
+
+  return (
+    <div className="container">
+      {products.map((product) => (
+        <div className="card_item" key={product.id}>
+          <div className="card_inner">
+            <img src={product.thumbnail} alt="" />
+            <div className="productName">{product.title}</div>
+            <div className="productPrice">${product.price}</div>
+            <div className="productQuantity">Quantity: {product.quantity}</div>
+            {/* Add other product details as needed */}
+            <button className="seeMore">See More</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
